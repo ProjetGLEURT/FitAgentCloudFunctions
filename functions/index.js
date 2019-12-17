9f8c90aea28ee2273bd0db856b206f418d1f4537
davidMcnJ <david.micouin-jorda@bordeaux-inp.fr>
2019-12-17 12:19:04 +0100

    addUser incomplet

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,81 @@ const {Card, Suggestion} = require('dialogflow-fulfillment');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
+
+
+//À CONFIGURER, À METTRE DANS UN FICHIER PUIS GITIGNORE
+var config = {
+    apiKey: "************************",
+    authDomain: "************************",
+    databaseURL: "************************",
+    projectId: "************************",
+    storageBucket: "************************",
+    messagingSenderId: "************************",
+};
+
+
+
+firebase.initializeApp(config);
+
+
+const dbRef = firebase.database().ref();
+const usersRef = dbRef.child('users');
+
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  function welcome(agent) {
+      agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+      agent.add(`I didn't understand`);
+      agent.add(`I'm sorry, can you try again?`);
+  }
+
+   // Uncomment and edit to make your own intent handler
+   // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+   // below to get this function to be run when a Dialogflow intent is matched
+   function addUserInfosToFirebase(agent) {
+    data = {infos:
+        {
+            nom:"test",
+            preference:"afternoon"
+        }
+    }
+    usersRef.push()
+    //const userRef = dbRef.child('users/' + e.target.getAttribute("userid"));
+     agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+     
+     agent.setContext({ name: 'activityRegistred', lifespan: 2, parameters: { city: 'Rome' }});
+   }
+
+
+   // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
+   // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('add User', addUserInfosToFirebase);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
+
+
+
+
+
+
+
+
+
+
+/*
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
     const agent = new WebhookClient({ request, response });
     console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
@@ -58,85 +133,4 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     // intentMap.set('your intent name here', yourFunctionHandler);
     // intentMap.set('your intent name here', googleAssistantHandler);
     agent.handleRequest(intentMap);
-});
-
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  const snapshot = await admin.database().ref('/messages').push({original: original});
-  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-  res.redirect(303, snapshot.ref.toString());
-});
-
-
-
-
-//Fonction addActivity
-
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-
-  function welcome(agent) {
-      agent.add(`Welcome to my agent!`);
-  }
-
-  function fallback(agent) {
-      agent.add(`I didn't understand`);
-      agent.add(`I'm sorry, can you try again?`);
-  }
-
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://assistant.google.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
-
-  // // Uncomment and edit to make your own Google Assistant intent handler
-  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function googleAssistantHandler(agent) {
-  //   let conv = agent.conv(); // Get Actions on Google library conv instance
-  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-  // }
-  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
-  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
-
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
-  // intentMap.set('your intent name here', yourFunctionHandler);
-  // intentMap.set('your intent name here', googleAssistantHandler);
-  agent.handleRequest(intentMap);
-});
-
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-// Grab the text parameter.
-const original = req.query.text;
-// Push the new message into the Realtime Database using the Firebase Admin SDK.
-const snapshot = await admin.database().ref('/addActivity').push({original: original});
-// Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-res.redirect(303, snapshot.ref.toString());
-});
+});*/
