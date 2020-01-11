4f7eb2f50d0c98dc5b4a70e3019eebf35b144492
Drovich <35914686+Drovich@users.noreply.github.com>
2020-01-11 17:05:16 +0100

    cleaned code dialogfloFullfillment

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,6 @@ const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
 const firebase = require('firebase');
 
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
 const keyApiGoogle = require("./keyApiGoogle.json");
 const googleMapsClient = require('@google/maps').createClient({
     key: keyApiGoogle,
@@ -30,6 +28,9 @@ const {
     getNumContext,
     computeSeanceDuration,
     } = require("./dialogflowFirebaseFulfillment/addUserActivityToFirebase")
+const {
+    HttpClient,
+} = require("./dialogflowFirebaseFulfillment/httpRequest")
 
 exports.addNewEventToCalendar = functions.database.ref('/users/{userId}/activities/{activityId}/events/{eventId}')
     .onCreate(async (snapshot, context) => {
@@ -145,58 +146,40 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
    function addUserActivityToFirebase(agent) {
 
-    var promesseRequeteUser = Promise.resolve(usersRef.orderByChild('infos/name').equalTo('david').once("value"));
+    let promesseRequeteUser = Promise.resolve(usersRef.orderByChild('infos/name').equalTo('david').once("value"));
 
-    console.log("TTESTT")
-    var HttpClient = function () {
-        this.get = function (aUrl, aCallback) {
-            var anHttpRequest = new XMLHttpRequest();
-            anHttpRequest.onreadystatechange = function () {
-                if (anHttpRequest.readyState === 4 && anHttpRequest.status === 200)
-                    aCallback(anHttpRequest.responseText);
-            }
-
-            anHttpRequest.open("GET", aUrl, true);
-            anHttpRequest.send(null);
-        }
-    }
-
-    console.log("TEST")
-    var client = new HttpClient();
+    let client = new HttpClient();
+    try{
     client.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?'+
     `key=${keyApiGoogle}&`+
     'input=bordeaux&'+
-    'inputtype=textquery', function (response) {
-        console.log("Réponse de la recherche")
+    'inputtype=textquery', response => {
+        console.log("Response of the request")
         console.log(response)
     });
+    }
+    catch(err){
+        throw new Error(`Request failed `, error)
+    }
+     return promesseRequeteUser.then(data => {
 
-    return promesseRequeteUser.then(data => {
-
-        var idUser = Object.keys(data.val())[0];
+        let idUser = Object.keys(data.val())[0];
         const myUserRef = usersRef.child(idUser);
         const myUserActsRef = myUserRef.child('activities');
 
 
 
-        console.log("BAH VOYONS")
+        console.log("Contexts of Dialogflow : ")
         console.log(agent.contexts)
 
-        var numContexte = getNumContext(agent, 'newactivity-followup');
-        var contextParameters = agent.contexts[numContexte].parameters;
-        console.log("numContexte : ")
-        console.log(numContexte)
-
-        var numContexteValidationDemandee = getNumContext(agent, 'new activity - yes') //contexte auquel en théorie, la variable sera rajoutée
+        let numContexte = getNumContext(agent, 'newactivity-followup');
+        let contextParameters = agent.contexts[numContexte].parameters;
+        let numContexteValidationDemandee = getNumContext(agent, 'new activity - yes') //context used to ask validation if activity already exist in the database
   
-        console.log("numContexteValidationDemandee : ")
-        console.log(numContexteValidationDemandee)
-
-
         // computeSeanceDuration 
-        var seanceDurationInMinute = computeSeanceDuration(contextParameters);
+        let seanceDurationInMinute = computeSeanceDuration(contextParameters);
 
-        var confirmationDemandee = false;
+        let confirmationDemandee = false;
         try{
             console.log("Contexte True : ")
             console.log(agent.contexts[numContexteValidationDemandee])
@@ -206,13 +189,14 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
             console.log("ERROR 1005 : ")
             console.log(error)
             confirmationDemandee = false
+            throw new Error(`Check dialogflow context `, error)
         }
         /*if(agent.contexts[agent.contexts.length-2]!==undefined && agent.contexts[agent.contexts.length-2].parameters.confirmationDemandee === true)
             confirmationDemandee = true*/
-        var nameSport = contextParameters.sport
+        let nameSport = contextParameters.sport
 
-        var homeTimeAmount = -1
-        var workTimeAmount = -1
+        let homeTimeAmount = -1
+        let workTimeAmount = -1
         if (contextParameters.homeTime !== undefined && contextParameters.homeTime.amount !== undefined)
             homeTimeAmount = contextParameters.homeTime.amount;
         if (contextParameters.workTime !== undefined && contextParameters.workTime.amount !== undefined)
@@ -222,7 +206,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
         if (addressToPush === undefined){
             addressToPush = "9 rue des inventions"
         }
-        var donnee = {
+        let donnee = {
             name: nameSport,
             placeType: contextParameters.placeType,
             address: addressToPush,
@@ -233,7 +217,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
             duration: seanceDurationInMinute,
         }
 
-        var promesseRequeteSport = Promise.resolve(myUserActsRef.orderByChild('name').equalTo(nameSport).once("value"));
+        let promesseRequeteSport = Promise.resolve(myUserActsRef.orderByChild('name').equalTo(nameSport).once("value"));
 
             return promesseRequeteSport.then(data => {
 
@@ -241,13 +225,11 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
             console.log("parameters", agent.contexts[0].parameters)
             if(data.val() === null || confirmationDemandee)
             {
-                agent.add(`WOOOW ON EST LÀ`);
-
+                agent.add(`Votre activité a été  `);
                 //type lieu = dehors, salle, chez soi
-
                 myUserActsRef.push(donnee)
                 //const userRef = dbRef.child('users/' + e.target.getAttribute("userid"));
-                agent.add(`OK COOOL : ${agent.contexts[0].parameters.sport}, ${agent.contexts[0].parameters.frequence}, ${seanceDurationInMinute} minutes`);
+                agent.add(`ajouté avec succès : ${agent.contexts[0].parameters.sport}, ${agent.contexts[0].parameters.frequence}, ${seanceDurationInMinute} minutes`);
 
                 agent.context.set({ name: 'New Activity', lifespan: 2, parameters: { }});
             }
@@ -261,14 +243,14 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
         })
         .catch(err => {
             console.log(err);
-            agent.add(`WOOOW BUG VERIF SPORT 1001`);
-            return 0;
+            agent.add(`ERROR votre activité n'a pas pu être ajouté. Désoler du dérangement`);
+            throw new Error("Activity can't be add to the database ",err)
         });
     })
     .catch(err => {
         console.log(err);
         agent.add(`WOOOW BUG 1000`);
-        return 0;
+        throw new Error("Check dialogflow contexts", err)
     });
    }
 
