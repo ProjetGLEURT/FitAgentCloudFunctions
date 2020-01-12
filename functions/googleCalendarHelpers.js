04442d89b9d9e637f15b068e45fad05c7e35febc
Bluedich <hugo.stinson@gmail.com>
2020-01-12 12:58:30 +0100

    Added function getFreeTimes that computes the free times for the specified user between the specified dates.

--- functions/googleCalendarHelpers.js
+++ functions/googleCalendarHelpers.js
@@ -69,3 +69,138 @@ async function deleteEvent(eventId, auth) {
         throw new Error('There was an error while deleting an event in google calendar: ' + err)
     }
 }
+
+exports.getFreeTimesFromGoogleCalendar = async function (token, timeMin, timeMax) {
+    const auth = await authorize(token);
+    let response = await getBusy(auth, timeMin, timeMax);
+    let busyTimes = combineBusyTimesFromCalendars(response);
+    busyTimes = convertIntervalsFromStringsToDates(busyTimes);
+    busyTimes = resolveOverlappingIntervals(busyTimes);
+
+    return getFreeTimes(busyTimes, timeMin, timeMax);
+};
+
+function combineBusyTimesFromCalendars(response) {
+    let busyTimes = [];
+    let calendars = response.data.calendars;
+    for (let calendar in calendars) {
+        // skip loop if the property is from prototype
+        if (!calendars.hasOwnProperty(calendar)) continue;
+        busyTimes = busyTimes.concat(calendars[calendar].busy || []);
+    }
+    return busyTimes;
+}
+
+function convertIntervalsFromStringsToDates(busyTimes) {
+    return busyTimes.map(interval => {
+        return {
+            start: Date.parse(interval.start),
+            end: Date.parse(interval.end)
+        }
+    })
+}
+
+function resolveOverlappingIntervals(busyTimes) {
+    busyTimes = sortIntervalsByStartTime(busyTimes);
+    let i = 0;
+    while (i < busyTimes.length - 1) {
+        let intervalEnd = busyTimes[i].end;
+        let lastIntervalToCombineIndex = getLastIntervalToCombineIndex(busyTimes, intervalEnd);
+        if (isNotOverlapping(busyTimes, i)) i++;
+        else busyTimes = combineIntervals(busyTimes, i, lastIntervalToCombineIndex);
+    }
+    return busyTimes;
+}
+
+// We assume that i < intervals.length - 1
+function getLastIntervalToCombineIndex(intervals, intervalEnd) {
+    let i = 0;
+    while (intervalEnd > intervals[i + 1].start) {
+        i++;
+    }
+    return i;
+}
+
+// We can assume that there is not overlap to the left. We thus only have to check overlap to the right.
+// We also assume that i < intervals.length - 1.
+function isNotOverlapping(intervals, i) {
+    return intervals[i].end < intervals[i + 1].start;
+}
+
+function sortIntervalsByStartTime(intervals) {
+    return intervals.sort((int1, int2) => int1.start > int2.start)
+}
+
+function combineIntervals(intervals, firstIntervalToCombineIndex, lastIntervalToCombineIndex) {
+    let newIntervals = intervals.slice(0, firstIntervalToCombineIndex);
+    newIntervals.push(
+        {
+            start: intervals[firstIntervalToCombineIndex].start,
+            end: getLatestEnd(intervals.slice(firstIntervalToCombineIndex, lastIntervalToCombineIndex + 1))
+        }
+    );
+    if (lastIntervalToCombineIndex === intervals.length - 1) {
+        return newIntervals;
+    }
+
+    return newIntervals.concat(intervals.slice(lastIntervalToCombineIndex + 1, intervals.length));
+}
+
+function getLatestEnd(intervals) {
+    return intervals.reduce((latestEnd, interval) => interval.end > latestEnd ? interval.end : interval, 0)
+}
+
+function getFreeTimes(busyTimes, timeMin, timeMax) {
+    let freeTimes = [
+        {
+            start: Date.parse(timeMin)
+        }
+    ];
+    busyTimes.forEach(busyInterval => {
+        freeTimes[freeTimes.length - 1].end = busyInterval.start;
+        freeTimes.push(
+            {
+                start: busyInterval.end
+            }
+        )
+    });
+    freeTimes[freeTimes.length - 1].end = Date.parse(timeMax);
+
+    return freeTimes;
+}
+
+async function getCalendarIdList(auth) {
+    const calendar = google.calendar({version: 'v3', auth});
+    let response;
+    try {
+        response = await calendar.calendarList.list();
+    } catch (err) {
+        throw new Error("Can't retrieve list of Google Calendars: " + err);
+    }
+    return getCalendarIds(response);
+}
+
+function getCalendarIds(response) {
+    return response.data.items.map(item => {
+        return {id: item.id}
+    });
+}
+
+async function getBusy(auth, timeMin, timeMax) {
+    const calendar = google.calendar({version: 'v3', auth});
+    let calendarIdList = await getCalendarIdList(auth);
+    let query = {
+        auth: auth,
+        resource: {
+            timeMin: timeMin,
+            timeMax: timeMax,
+            timeZone: 'Europe/Paris',
+            items: calendarIdList
+        }
+    };
+    try {
+        return await calendar.freebusy.query(query);
+    } catch (err) {
+        throw new Error('There was an error while requesting freebusy info from Google Calendar: ' + err)
+    }
+}
--- functions/index.js
+++ functions/index.js
@@ -19,15 +19,10 @@ firebase.initializeApp(firebaseConfig);
 const dbRef = firebase.database().ref();
 const usersRef = dbRef.child('users');
 
-const {
-    getNumContext,
-    computeSeanceDuration,
-    } = require("./dialogflowFirebaseFulfillment/addUserActivityToFirebase")
-const {
-    HttpClient,
-} = require("./dialogflowFirebaseFulfillment/httpRequest")
-
-const {addNewEventToGoogleCalendar, setEventData, deleteEventFromGoogleCalendar} = require('./googleCalendarHelpers');
+const {getNumContext, computeSeanceDuration} = require("./dialogflowFirebaseFulfillment/addUserActivityToFirebase");
+const {HttpClient} = require("./dialogflowFirebaseFulfillment/httpRequest");
+const {addNewEventToGoogleCalendar, setEventData, deleteEventFromGoogleCalendar, getFreeTimesFromGoogleCalendar}
+    = require('./googleCalendarHelpers');
 
 /**
  * When a new event is created in Firebase, creates a new event in the corresponding user's Google Calendar. The id of
@@ -97,9 +92,15 @@ exports.deleteEventFromCalendar = functions.database
         await deleteEventFromGoogleCalendar(googleEventId, token);
     });
 
+/**
+ * Returns the list of the free time intervals of the user over the specified period.
+ */
+async function getFreeTimes(token, timeMin, timeMax) {
+    return await getFreeTimesFromGoogleCalendar(token, timeMin, timeMax);
+}
+
 exports.apiSupprimerActiviteUser = functions.https.onRequest((request, response) => {
     var id = request.query.id;
-
 
     var promesseRequeteUser = Promise.resolve(usersRef.orderByChild('infos/name').equalTo('david').once("value"));
 
@@ -191,120 +192,116 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     }
 
 
-   function addUserActivityToFirebase(agent) {
+    function addUserActivityToFirebase(agent) {
 
-    let promesseRequeteUser = Promise.resolve(usersRef.orderByChild('infos/name').equalTo('david').once("value"));
+        let promesseRequeteUser = Promise.resolve(usersRef.orderByChild('infos/name').equalTo('david').once("value"));
 
 
-    let client = new HttpClient();
-    try{
-    client.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?'+
-    `key=${keyApiGoogle}&`+
-    'input=bordeaux&'+
-    'inputtype=textquery', response => {
-        console.log("Response of the request")
-        console.log(response)
-    });
-    }
-    catch(err){
-        throw new Error(`Request failed `, error)
-    }
-     return promesseRequeteUser.then(data => {
+        let client = new HttpClient();
+        try {
+            client.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?' +
+                `key=${keyApiGoogle}&` +
+                'input=bordeaux&' +
+                'inputtype=textquery', response => {
+                console.log("Response of the request")
+                console.log(response)
+            });
+        } catch (err) {
+            throw new Error(`Request failed `, error)
+        }
+        return promesseRequeteUser.then(data => {
 
-        let idUser = Object.keys(data.val())[0];
-        const myUserRef = usersRef.child(idUser);
-        const myUserActsRef = myUserRef.child('activities');
+            let idUser = Object.keys(data.val())[0];
+            const myUserRef = usersRef.child(idUser);
+            const myUserActsRef = myUserRef.child('activities');
 
 
             console.log("BAH VOYONS")
             console.log(agent.contexts)
 
-        console.log("Contexts of Dialogflow : ")
-        console.log(agent.contexts)
+            console.log("Contexts of Dialogflow : ")
+            console.log(agent.contexts)
 
-        let numContexte = getNumContext(agent, 'newactivity-followup');
-        let contextParameters = agent.contexts[numContexte].parameters;
-        let numContexteValidationDemandee = getNumContext(agent, 'new activity - yes') //context used to ask validation if activity already exist in the database
+            let numContexte = getNumContext(agent, 'newactivity-followup');
+            let contextParameters = agent.contexts[numContexte].parameters;
+            let numContexteValidationDemandee = getNumContext(agent, 'new activity - yes') //context used to ask validation if activity already exist in the database
 
-        // computeSeanceDuration
-        let seanceDurationInMinute = computeSeanceDuration(contextParameters);
+            // computeSeanceDuration
+            let seanceDurationInMinute = computeSeanceDuration(contextParameters);
 
-        let confirmationDemandee = false;
-        try{
-            console.log("Contexte True : ")
-            console.log(agent.contexts[numContexteValidationDemandee])
-            confirmationDemandee = agent.contexts[numContexteValidationDemandee].parameters.confirmationDemandee;
-        }
-        catch(error){
-            console.log("ERROR 1005 : ")
-            console.log(error)
-            confirmationDemandee = false
-            throw new Error(`Check dialogflow context `, error)
-        }
-        /*if(agent.contexts[agent.contexts.length-2]!==undefined && agent.contexts[agent.contexts.length-2].parameters.confirmationDemandee === true)
-            confirmationDemandee = true*/
-        let nameSport = contextParameters.sport
+            let confirmationDemandee = false;
+            try {
+                console.log("Contexte True : ")
+                console.log(agent.contexts[numContexteValidationDemandee])
+                confirmationDemandee = agent.contexts[numContexteValidationDemandee].parameters.confirmationDemandee;
+            } catch (error) {
+                console.log("ERROR 1005 : ")
+                console.log(error)
+                confirmationDemandee = false
+                throw new Error(`Check dialogflow context `, error)
+            }
+            /*if(agent.contexts[agent.contexts.length-2]!==undefined && agent.contexts[agent.contexts.length-2].parameters.confirmationDemandee === true)
+                confirmationDemandee = true*/
+            let nameSport = contextParameters.sport
 
-        let homeTimeAmount = -1
-        let workTimeAmount = -1
-        if (contextParameters.homeTime !== undefined && contextParameters.homeTime.amount !== undefined)
-            homeTimeAmount = contextParameters.homeTime.amount;
-        if (contextParameters.workTime !== undefined && contextParameters.workTime.amount !== undefined)
-            workTimeAmount = contextParameters.workTime.amount;
+            let homeTimeAmount = -1
+            let workTimeAmount = -1
+            if (contextParameters.homeTime !== undefined && contextParameters.homeTime.amount !== undefined)
+                homeTimeAmount = contextParameters.homeTime.amount;
+            if (contextParameters.workTime !== undefined && contextParameters.workTime.amount !== undefined)
+                workTimeAmount = contextParameters.workTime.amount;
 
-        let addressToPush = contextParameters.address
-        if (addressToPush === undefined){
-            addressToPush = "9 rue des inventions"
-        }
-        let donnee = {
-            name: nameSport,
-            placeType: contextParameters.placeType,
-            address: addressToPush,
-            homeTime:  homeTimeAmount,
-            workTime:  workTimeAmount,
-            frequence: contextParameters.frequence,
-            nbSeance: contextParameters.nbSeance,
-            duration: seanceDurationInMinute,
-        }
+            let addressToPush = contextParameters.address
+            if (addressToPush === undefined) {
+                addressToPush = "9 rue des inventions"
+            }
+            let donnee = {
+                name: nameSport,
+                placeType: contextParameters.placeType,
+                address: addressToPush,
+                homeTime: homeTimeAmount,
+                workTime: workTimeAmount,
+                frequence: contextParameters.frequence,
+                nbSeance: contextParameters.nbSeance,
+                duration: seanceDurationInMinute,
+            }
 
-        let promesseRequeteSport = Promise.resolve(myUserActsRef.orderByChild('name').equalTo(nameSport).once("value"));
+            let promesseRequeteSport = Promise.resolve(myUserActsRef.orderByChild('name').equalTo(nameSport).once("value"));
 
             return promesseRequeteSport.then(data => {
 
-            console.log("data", data.val())
-            console.log("parameters", agent.contexts[0].parameters)
-            if(data.val() === null || confirmationDemandee)
-            {
-                agent.add(`Votre activité a été  `);
-                //type lieu = dehors, salle, chez soi
-                myUserActsRef.push(donnee)
-                //const userRef = dbRef.child('users/' + e.target.getAttribute("userid"));
-                agent.add(`ajouté avec succès : ${agent.contexts[0].parameters.sport}, ${agent.contexts[0].parameters.frequence}, ${seanceDurationInMinute} minutes`);
+                console.log("data", data.val())
+                console.log("parameters", agent.contexts[0].parameters)
+                if (data.val() === null || confirmationDemandee) {
+                    agent.add(`Votre activité a été  `);
+                    //type lieu = dehors, salle, chez soi
+                    myUserActsRef.push(donnee)
+                    //const userRef = dbRef.child('users/' + e.target.getAttribute("userid"));
+                    agent.add(`ajouté avec succès : ${agent.contexts[0].parameters.sport}, ${agent.contexts[0].parameters.frequence}, ${seanceDurationInMinute} minutes`);
 
-                agent.context.set({ name: 'New Activity', lifespan: 2, parameters: { }});
-            }
-            else{
+                    agent.context.set({name: 'New Activity', lifespan: 2, parameters: {}});
+                } else {
 
-                donnee.confirmationDemandee = true;
-                agent.context.set({ name: 'new activity - yes', lifespan: 2, parameters: donnee });
-                agent.add(`Le sport que vous souhaitez ajouter possède déjà des activités, voulez-vous confirmer votre ajout ?`);
-            }
-            return 0;
+                    donnee.confirmationDemandee = true;
+                    agent.context.set({name: 'new activity - yes', lifespan: 2, parameters: donnee});
+                    agent.add(`Le sport que vous souhaitez ajouter possède déjà des activités, voulez-vous confirmer votre ajout ?`);
+                }
+                return 0;
+            })
+                .catch(err => {
+                    console.log(err);
+                    agent.add(`ERROR votre activité n'a pas pu être ajouté. Désoler du dérangement`);
+                    throw new Error("Activity can't be add to the database ", err)
+                });
         })
-        .catch(err => {
-            console.log(err);
-            agent.add(`ERROR votre activité n'a pas pu être ajouté. Désoler du dérangement`);
-            throw new Error("Activity can't be add to the database ",err)
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        agent.add(`WOOOW BUG 1000`);
-        throw new Error("Check dialogflow contexts", err)
-    });
-   }
+            .catch(err => {
+                console.log(err);
+                agent.add(`WOOOW BUG 1000`);
+                throw new Error("Check dialogflow contexts", err)
+            });
+    }
 
-   function guessedAddress () {
+    function guessedAddress() {
         var guessedAddress = "9 rue des inventions";
         agent.context.set({name: 'new activity - address', lifespan: 2, parameters: {guessedAddress: guessedAddress}});
         agent.add(`Nous vous suggérons de faire votre activité à ${guessedAddress}, cela vous convient-il ?`);
