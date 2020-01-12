cdf644bc5e7d88471b7c5a475b3736c70e56614c
Drovich <35914686+Drovich@users.noreply.github.com>
2020-01-12 01:27:43 +0100

    Google Auth token in context of dialogflow

--- functions/dialogflowFirebaseFulfillment/addUserActivityToFirebase.js
+++ functions/dialogflowFirebaseFulfillment/addUserActivityToFirebase.js
@@ -1,13 +1,25 @@
-exports.getNumContext = function  (agent,name) {
-    let numContexte = 0
+let getNumContext = function  (agent,name) {
+    let numContext = 0
     for(let i = 0; i < agent.contexts.length;i++){
-        console.log(agent.contexts[i]);
         if(agent.contexts[i].name === name){
             numContexte = i;
-            return numContexte
+            return numContext
         }
     }
-    return numContexte
+    return numContext
+}
+
+exports.getContextParameters = function (agent, name) {
+    return agent.contexts[getNumContext(agent,name)].parameters
+}
+
+exports.getToken = function (agent) {
+    let numContext = getNumContext(agent, 'test11');
+    let contextParameters = agent.contexts[numContext].parameters;
+    if (contextParameters !== undefined && contextParameters.token !== undefined) {
+        return contextParameters.token
+    }
+    return ''
 }
 
 exports.computeSeanceDuration = function (contextParameters) {
